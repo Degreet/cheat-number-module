42a26fa143dbe65fdcf73e3600cca03bda7d38d7
Degreet <degreetpro@gmail.com>
2021-05-09 22:52:39 +0300

    to random

--- js/example.js
+++ js/example.js
@@ -1,10 +1,17 @@
+const from = 10;
+const to = rndNum(100, 400);
+
+function rndNum(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
 cheatNumber({
-	from: 10,
-	to: 345,
-	interval: 7,
+	from,
+	to,
+	interval: 10,
 	customInterval: {
 		startOn: 10,
-		interval: 50,
+		interval: 140,
 	},
 	callback: (count, isEnded) => {
 		cheatNumberEl.innerText = count;
